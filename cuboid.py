c7f7996b3fedd776885a58a4d025b037d64770d1
GHANASYAM RAVEENDRAN <93483011+rghanasyam98@users.noreply.github.com>
2022-01-19 11:30:14 +0530

    Add files via upload

---
+++ cuboid.py
@@ -0,0 +1,4 @@
+def cuboidsurarea(l,b,h):
+    return 2*((l*b)+(l*h)+(b*h))
+def cuboidper(l,b,h):
+    return 4*(l+b+h)
---
+++ sphere.py
@@ -0,0 +1,5 @@
+import math
+def spherecircum(r):
+    return 2*math.pi*r
+def spheresurarea(r):
+    return 4*math.pi*r*r
